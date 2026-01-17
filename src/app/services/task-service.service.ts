b386631be566e23f8ce07ce95587fee0f71c5d8e
hadjer <hadjer.megoura@wissalgroup.com>
2026-01-17 07:40:56 +0100

    update firebase databse link

--- src/app/services/task-service.service.ts
+++ src/app/services/task-service.service.ts
@@ -6,58 +6,72 @@ import { LoggingService } from './logging.service';
 import { throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
-
   http: HttpClient = inject(HttpClient);
   allTasks: Task[] = [];
   loggingService: LoggingService = inject(LoggingService);
 
-  constructor() { }
+  constructor() {}
 
   CreateTask(taskData: Task) {
-    return this.http.post<({name: string})>
-    ('https://angularhttpclient-ac5df-default-rtdb.firebaseio.com/Tasks.json'
-    ,taskData);
+    return this.http.post<{ name: string }>(
+      'https://my-project-a8d57-default-rtdb.firebaseio.com/Tasks.json',
+      taskData,
+    );
   }
   GetAllTasks() {
-     return this.http.get<{[key: string]: Task}>
-        ('https://angularhttpclient-ac5df-default-rtdb.firebaseio.com/Tasks.json')
-        .pipe(map((response) => {
+    return this.http
+      .get<{
+        [key: string]: Task;
+      }>('https://my-project-a8d57-default-rtdb.firebaseio.com/Tasks.json')
+      .pipe(
+        map((response) => {
           //Transform data into array
           let tasks = [];
-    
-          for(let key in response){
+
+          for (let key in response) {
             if (response.hasOwnProperty(key)) {
-                 //add to the table the object response, with an id which is the key 
-            //(...) to extract the proprties and count them as the new object proprties
-              tasks.push({...response[key], id: key})
-            }           
+              //add to the table the object response, with an id which is the key
+              //(...) to extract the proprties and count them as the new object proprties
+              tasks.push({ ...response[key], id: key });
+            }
           }
           return tasks;
-    
-        })
-        ,catchError((error) => {
-           const errorObject = {statusCode: error.status, errorMessage: error.message, dateTime: new Date()}
-           this.loggingService.logErrors(errorObject);
-             return throwError(()=>error);
-        })
+        }),
+        catchError((error) => {
+          const errorObject = {
+            statusCode: error.status,
+            errorMessage: error.message,
+            dateTime: new Date(),
+          };
+          this.loggingService.logErrors(errorObject);
+          return throwError(() => error);
+        }),
       );
-
-
-       
   }
   RemoveTask(taskId: string | undefined) {
-   return this.http.delete('https://angularhttpclient-ac5df-default-rtdb.firebaseio.com/Tasks/'+taskId+'.json');
+    return this.http.delete(
+      'https://my-project-a8d57-default-rtdb.firebaseio.com/Tasks/' +
+        taskId +
+        '.json',
+    );
   }
   ClearAllTasks() {
-    return this.http.delete('https://angularhttpclient-ac5df-default-rtdb.firebaseio.com/Tasks/.json');
+    return this.http.delete(
+      'https://my-project-a8d57-default-rtdb.firebaseio.com/Tasks/.json',
+    );
   }
 
   UpdateTask(taskId: string | undefined, data: Task) {
-    this.http.put('https://angularhttpclient-ac5df-default-rtdb.firebaseio.com/Tasks/'+taskId+'.json',data)
-    .subscribe();
-
+    this.http
+      .put(
+        'https://my-project-a8d57-default-rtdb.firebaseio.com/Tasks/' +
+          taskId +
+          '.json',
+        data,
+      )
+      .subscribe();
   }
 }
